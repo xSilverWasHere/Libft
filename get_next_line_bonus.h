21b2e5d78b3b0616445d2f9dbd2ece7158a74383
Silver <fireon42@gmail.com>
2025-06-13 16:38:51 +0100

    added get_next_line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedro-g <jpedro-g@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 12:35:29 by jpedro-g          #+#    #+#             */
+/*   Updated: 2025/05/15 11:33:47 by jpedro-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 1024
+
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+//main functions
+char	*get_next_line(int fd);
+
+//utils
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strndup(const char *s, size_t len);
+char	*ft_strdup(char *s);
+
+#endif
